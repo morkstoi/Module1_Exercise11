a943cd7a544744e5ab3608e9cba522ef9dcde41d
theac <theac@DESKTOP-LEARA2L>
2024-01-08 18:33:55 +0300

    Add project files.

---
+++ LinearEquasion/Linear.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LinearEquasion
+{
+    struct Linear
+    {
+        double k;
+        double b;
+
+        public Linear (double k, double b)
+        {
+            this.k = k;
+            this.b = b;
+
+        }
+
+        public string Root()
+        {
+            if (k != 0 && b != 0)
+            {
+                double solution = -b / k;
+                return $"Решение: {solution}";
+            }
+            else if (k == 0 && b != 0)
+                return "Решения нет.";
+            else
+                return "Бесконечное количество корней.";
+
+        }
+    }
+}
---
+++ LinearEquasion/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LinearEquasion
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double k = Convert.ToInt32(Console.ReadLine());
+            double b = Convert.ToInt32(Console.ReadLine());
+
+            Linear linear = new Linear(k, b);
+            Console.WriteLine(linear.Root());
+            Console.ReadKey();
+        }
+    }
+}
